630b12a90a1ff045b2f0416f1e131f60b81d94d0
Matthias Mandler <me@motz.xyz>
2025-07-08 12:14:09 +0200

    Create BalanceUpdateEvent.java

---
+++ src/main/java/dev/unnm3d/rediseconomy/api/BalanceUpdateEvent.java
@@ -0,0 +1,26 @@
+package dev.unnm3d.rediseconomy.api;
+
+import dev.unnm3d.rediseconomy.transaction.AccountID;
+import dev.unnm3d.rediseconomy.transaction.Transaction;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+@AllArgsConstructor
+@Getter
+public class BalanceUpdateEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private AccountID accountID;
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
